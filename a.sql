a7b3632e1d332472543581d77e39b0871afd3b45
chetambrat <chetambrat@users.noreply.github.com>
2021-02-17 16:12:18 +0300

    dsaasdads

--- a.sql
+++ a.sql
@@ -5,10 +5,10 @@ ALTER TABLE "mvd_users" DROP CONSTRAINT proper_phone;
 ALTER TABLE "lohs" DROP CONSTRAINT proper_passport;
 
 ALTER TABLE "mvd_users"
-ADD CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}+$');
+ADD CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$');
 
 ALTER TABLE "mvd_users"
-ADD CONSTRAINT proper_phone CHECK (phone ~* '^+79[0-9]{9}$');
+ADD CONSTRAINT proper_phone CHECK (phone ~* '^\+79[0-9]{9}$');
 
 ALTER TABLE "lohs"
-ADD CONSTRAINT proper_passport CHECK (lohs.passportSerialNumber ~* '^[0-9]{10}$');
+ADD CONSTRAINT proper_passport CHECK (passportSerialNumber ~* '^[0-9]{10}$');
