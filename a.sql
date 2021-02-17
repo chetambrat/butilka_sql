6e0c985db43378a8f17f58076d72a7373520226b
chetambrat <chetambrat@users.noreply.github.com>
2021-02-17 16:24:56 +0300

    asd

--- a.sql
+++ a.sql
@@ -4,6 +4,8 @@ ALTER TABLE "mvd_users" DROP CONSTRAINT proper_phone;
 
 ALTER TABLE "lohs" DROP CONSTRAINT proper_passport;
 
+ALTER TABLE "lohs" DROP CONSTRAINT proper_sub_code;
+
 ALTER TABLE "mvd_users"
 ADD CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]{2,}$');
 
@@ -11,4 +13,13 @@ ALTER TABLE "mvd_users"
 ADD CONSTRAINT proper_phone CHECK (phone ~* '^\+79[0-9]{9}$');
 
 ALTER TABLE "lohs"
-ADD CONSTRAINT proper_passport CHECK (passportSerialNumber ~* '^[0-9]{10}$');
+ADD CONSTRAINT proper_passport CHECK (passport_serial_number ~* '^[0-9]{10}$');
+
+ALTER TABLE "lohs"
+ADD CONSTRAINT proper_sub_code CHECK (subdivision_code ~* '^[0-9]{3}-[0-9]{3}$');
+
+ALTER TABLE "mvd_users"
+ADD UNIQUE (username);
+
+ALTER TABLE "lohs"
+ADD UNIQUE (passport_serial_number);
