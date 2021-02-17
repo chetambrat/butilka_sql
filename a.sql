7a38c21033d2f05c4c7222fc58e8edc43fc8c1a5
chetambrat <chetambrat@users.noreply.github.com>
2021-02-17 15:28:25 +0300

    dasads

--- a.sql
+++ a.sql
@@ -1,8 +1,8 @@
 ALTER TABLE "mvd_users"
-ADD CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
+ADD CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}+$');
 
 ALTER TABLE "mvd_users"
 ADD CONSTRAINT proper_phone CHECK (phone ~* '^+79[0-9]{9}$');
 
 ALTER TABLE "lohs"
-ADD CONSTRAINT proper_passport CHECK (passportSerialNumber ~* '^+[0-9]{10}$');
+ADD CONSTRAINT proper_passport CHECK (passportSerialNumber ~* '^[0-9]{10}$');
